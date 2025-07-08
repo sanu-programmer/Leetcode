e4a327c065f5d72919e71e837ceadf6a08dc8990
Sanu Kumar <Sanuk8137@gmail.com>
2025-07-08 10:01:05 +0530

    3rd problem done!!

---
+++ 169.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+        int  n = nums.size();
+
+        int candidate , count = 0 ;
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(count == 0 )
+            {
+                count = 1 ;
+                candidate = nums[i];
+            }
+            else
+            {
+                if(candidate == nums[i])
+                count++;
+                else
+                count--;
+            }
+        }
+        return candidate;
+    }
